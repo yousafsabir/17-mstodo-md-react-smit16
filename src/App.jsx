02582dc2767dccc6415fd902a7771df1bbab4d38
yousafsabir <yousafsabir430516@gmail.com>
2022-03-24 21:11:59 +0500

    made sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import menu from "./assets/menu.svg";
-import settings from "./assets/settings.svg";
-import question from "./assets/question.svg";
-import notification from "./assets/notifications.svg";
-
 import "./App.css";
+import MainBody from "./components/mainBody/MainBody";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="todoApp">
-      <nav className="navbar">
-        <div className="navLeft">
-          <img src={menu} className="navIcon navMenu" />
-          <h1 className="appName">To Do</h1>
-        </div>
-        <div className="navMiddle">
-          <input type="search" className="navSearch" />
-        </div>
-        <div className="navRight">
-          <img src={settings} className="navIcon navSettings" />
-          <img src={question} className="navIcon navQuestion" />
-          <img src={notification} className="navIcon navNotification" />
-          <div className="userProfile">
-            <p>MS</p>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
+      <MainBody />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import menu from "../../assets/menu.svg";
+import settings from "../../assets/settings.svg";
+import question from "../../assets/question.svg";
+import notification from "../../assets/notifications.svg";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navLeft">
+        <img src={menu} className="navIcon navMenu" />
+        <h1 className="appName">To Do</h1>
+      </div>
+      <div className="navMiddle">
+        <input type="search" className="navSearch" />
+      </div>
+      <div className="navRight">
+        <img src={settings} className="navIcon navSettings" />
+        <img src={question} className="navIcon navQuestion" />
+        <img src={notification} className="navIcon navNotification" />
+        <div className="userProfile">
+          <p>YS</p>
+        </div>
+      </div>
+    </nav>
+  );
+}
